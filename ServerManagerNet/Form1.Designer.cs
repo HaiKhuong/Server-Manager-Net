2bdf5c41761e2208d1747307cebc504132a2826f
haikhuong <khuong.nguyen@itbeesolutions.vn>
2021-05-15 21:51:43 +0700

    Add project files.

---
+++ ServerManagerNet/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace ServerManagerNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "May 1",
+            "Nap 1.000.000vnd"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+            "May 2",
+            "Strongbow"}, -1);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabHome = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.colClient = new System.Windows.Forms.ColumnHeader();
+            this.colService = new System.Windows.Forms.ColumnHeader();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabData = new System.Windows.Forms.TabPage();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lbNumberPage = new System.Windows.Forms.Label();
+            this.btnNextPage = new System.Windows.Forms.Button();
+            this.btnPrevPage = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.cbxListTable = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tabChat = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabHome.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabData.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.tabChat.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabHome);
+            this.tabControl1.Controls.Add(this.tabData);
+            this.tabControl1.Controls.Add(this.tabChat);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(785, 453);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabHome
+            // 
+            this.tabHome.Controls.Add(this.panel2);
+            this.tabHome.Controls.Add(this.panel1);
+            this.tabHome.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tabHome.Location = new System.Drawing.Point(4, 24);
+            this.tabHome.Name = "tabHome";
+            this.tabHome.Padding = new System.Windows.Forms.Padding(3);
+            this.tabHome.Size = new System.Drawing.Size(777, 425);
+            this.tabHome.TabIndex = 0;
+            this.tabHome.Text = "HOME";
+            this.tabHome.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(439, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(335, 419);
+            this.panel2.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colClient,
+            this.colService});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem3,
+            listViewItem4});
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(335, 419);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // colClient
+            // 
+            this.colClient.Text = "Client";
+            this.colClient.Width = 100;
+            // 
+            // colService
+            // 
+            this.colService.Text = "Service";
+            this.colService.Width = 230;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(436, 419);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(33, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(337, 336);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabData
+            // 
+            this.tabData.Controls.Add(this.panel5);
+            this.tabData.Controls.Add(this.panel4);
+            this.tabData.Controls.Add(this.panel3);
+            this.tabData.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tabData.Location = new System.Drawing.Point(4, 24);
+            this.tabData.Name = "tabData";
+            this.tabData.Padding = new System.Windows.Forms.Padding(3);
+            this.tabData.Size = new System.Drawing.Size(777, 425);
+            this.tabData.TabIndex = 1;
+            this.tabData.Text = "DATA";
+            this.tabData.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.dataGridView1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(3, 49);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(771, 335);
+            this.panel5.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(771, 335);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.lbNumberPage);
+            this.panel4.Controls.Add(this.btnNextPage);
+            this.panel4.Controls.Add(this.btnPrevPage);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(3, 384);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(771, 38);
+            this.panel4.TabIndex = 1;
+            // 
+            // lbNumberPage
+            // 
+            this.lbNumberPage.AutoSize = true;
+            this.lbNumberPage.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbNumberPage.Location = new System.Drawing.Point(374, 10);
+            this.lbNumberPage.Name = "lbNumberPage";
+            this.lbNumberPage.Size = new System.Drawing.Size(17, 19);
+            this.lbNumberPage.TabIndex = 2;
+            this.lbNumberPage.Text = "1";
+            // 
+            // btnNextPage
+            // 
+            this.btnNextPage.Location = new System.Drawing.Point(412, 6);
+            this.btnNextPage.Name = "btnNextPage";
+            this.btnNextPage.Size = new System.Drawing.Size(75, 27);
+            this.btnNextPage.TabIndex = 1;
+            this.btnNextPage.Text = "Next";
+            this.btnNextPage.UseVisualStyleBackColor = true;
+            // 
+            // btnPrevPage
+            // 
+            this.btnPrevPage.Location = new System.Drawing.Point(276, 6);
+            this.btnPrevPage.Name = "btnPrevPage";
+            this.btnPrevPage.Size = new System.Drawing.Size(75, 27);
+            this.btnPrevPage.TabIndex = 0;
+            this.btnPrevPage.Text = "Prev";
+            this.btnPrevPage.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnSubmit);
+            this.panel3.Controls.Add(this.tbSearch);
+            this.panel3.Controls.Add(this.cbxListTable);
+            this.panel3.Controls.Add(this.btnSearch);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(771, 46);
+            this.panel3.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(261, 9);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(90, 27);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tbSearch.Location = new System.Drawing.Point(357, 9);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(314, 26);
+            this.tbSearch.TabIndex = 2;
+            // 
+            // cbxListTable
+            // 
+            this.cbxListTable.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cbxListTable.FormattingEnabled = true;
+            this.cbxListTable.Location = new System.Drawing.Point(5, 9);
+            this.cbxListTable.Name = "cbxListTable";
+            this.cbxListTable.Size = new System.Drawing.Size(238, 27);
+            this.cbxListTable.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSearch.Location = new System.Drawing.Point(677, 9);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(89, 27);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // tabChat
+            // 
+            this.tabChat.Controls.Add(this.textBox1);
+            this.tabChat.Location = new System.Drawing.Point(4, 24);
+            this.tabChat.Name = "tabChat";
+            this.tabChat.Size = new System.Drawing.Size(777, 425);
+            this.tabChat.TabIndex = 2;
+            this.tabChat.Text = "CHAT";
+            this.tabChat.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(777, 425);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 453);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabHome.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabData.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.tabChat.ResumeLayout(false);
+            this.tabChat.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabHome;
+        private System.Windows.Forms.TabPage tabData;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnNextPage;
+        private System.Windows.Forms.Button btnPrevPage;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.ComboBox cbxListTable;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader colClient;
+        private System.Windows.Forms.ColumnHeader Service;
+        private System.Windows.Forms.ColumnHeader colService;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TabPage tabChat;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbNumberPage;
+    }
+}
+
---
+++ ServerManagerNet/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace ServerManagerNet
+{
+    public partial class Form1 : Form
+    {
+        public string connectString = @"Data Source=Localhost;Initial Catalog=ManagerNET;Integrated Security=True";
+
+        int recv;
+        IPEndPoint ipep;
+        Socket server;
+        byte[] data = new byte[1024];
+        byte[] dataSend = new byte[1024];
+        List<Socket> clientList;
+        public Form1()
+        {
+            InitializeComponent();
+            Connect();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetDataCBX();
+            dataGridView1.DataSource = GetAllListHoiVien().Tables[0];
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Test();
+            dataGridView1.DataSource = GetAllMenu(cbxListTable.SelectedItem.ToString().Trim()).Tables[0];
+        }
+        void SetDataCBX()
+        {
+            string query = "select * from DanhSachMenu";
+            using (SqlConnection connection = new SqlConnection(connectString))
+            {
+                connection.Open();
+                SqlCommand sqlc = new SqlCommand(query, connection);
+                SqlDataReader adapter = sqlc.ExecuteReader();
+                while (adapter.Read())
+                {
+                    cbxListTable.Items.Add(adapter.GetString("name").Trim());
+                }
+                adapter.Close();
+                connection.Close();
+                cbxListTable.SelectedIndex = 0;
+            }
+        }
+        DataSet GetAllMenu(string x)
+        {
+            string s = x.Replace(" ", "");
+            DataSet data = new DataSet();
+            string query = "select * from " + s;
+            using(SqlConnection connection = new SqlConnection(connectString))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+        DataSet GetAllListHoiVien()
+        {
+            DataSet data = new DataSet();
+            string query = "select * from DanhSachHoiVien";
+            using (SqlConnection connection = new SqlConnection(connectString))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+
+        void Connect()
+        {
+            clientList = new List<Socket>();
+            ipep = new IPEndPoint(IPAddress.Any, 9050);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            server.Bind(ipep);
+            Thread listen = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        server.Listen(100);
+                        Socket client = server.Accept();
+                        clientList.Add(client);
+
+                        Thread recvice = new Thread(Recv);
+                        recvice.IsBackground = true;
+                        recvice.Start(client);
+                    }
+                }
+                catch {
+                    ipep = new IPEndPoint(IPAddress.Any, 9050);
+                    server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                }
+            });
+            listen.IsBackground = true;
+            listen.Start();
+        }
+
+        void Send(Socket client)
+        {
+
+        }
+
+        void Recv(object obj)
+        {
+            Socket client = obj as Socket;
+            try
+            {
+                while(true)
+                {
+
+                    recv = client.Receive(data);
+                    string s = Encoding.ASCII.GetString(data, 0, recv);
+                    string[] ss = s.Split(":");
+
+                    if(ss[0] == "order")
+                    {
+                        string[] row1 = { ss[2] };
+                        listView1.Items.Add(ss[1]).SubItems.AddRange(row1);
+                        listView1.EnsureVisible(listView1.Items.Count - 1);
+                    }
+                    if (ss[0] == "login")
+                    {
+                        MessageBox.Show("LOGIN");
+                    }
+                    if (ss[0] == "recharge")
+                    {
+                        MessageBox.Show("NAP TIEN");
+                    }
+                    if (ss[0] == "support")
+                    {
+                        MessageBox.Show("SUPPORT");
+                    }
+                    if (ss[0] == "chat")
+                    {
+                        textBox1.Text += "\n" + ss[1].Trim() + ": " + ss[2].Trim();
+                    }
+
+                }
+            }
+            catch { }
+        }
+        void Test()
+        {
+            string[] row1 = { "Sửa Chua" };
+            listView1.Items.Add("Test").SubItems.AddRange(row1);
+        }
+    }
+}
